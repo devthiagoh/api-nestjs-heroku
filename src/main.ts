fa0d771fc6d6e8128d5a6ad98bf3f0ff97a36b9b
Thiago Leite <devthiagoh@gmail.com>
2021-11-13 03:30:08 -0300

    PORT

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { env } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
